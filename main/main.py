87a078d0a7817ed8dc5c0c7cd2360dfd4dee76c8
Theodicy1 <78248195+xxruyle@users.noreply.github.com>
2022-03-29 23:42:41 -0500

    added comments and functions
    added the new functions from random_passage.py

--- main/main.py
+++ main/main.py
@@ -1,31 +1,30 @@
 from readbible import data  # The bible in json format
 from tokenize_passage import parse 
 from random_passage import randompassage
-    #: = parsed_passage[2]
-    #init_verse = parsed_passage[3]
-    #- = parsed_passage[4]
-
+from random_passage import randombook_passage
+from random_passage import books 
 
 def getverse(passage):
     parsed_passage = parse(passage)  
     book = parsed_passage[0]
     chapter = parsed_passage[1]
     obt_passage = []  # list for storing passages that have multiple lines 
-    # if parse length is 2 (EX: Genesis 1)
+    
     try:
+        # if parse length is 2 (EX: Genesis 1)
         if len(parsed_passage) == 2:
             multiverse = data[book][chapter]  # getting the parse
             for verse in multiverse:  
                 obt_passage.append(verse + ' ' + multiverse[verse])  # getting proper format and adding the lines to the list
             return readlist(obt_passage)  # returning the passage list      
-        # if parse length is 4 (EX: Genesis 1:2)
+        # if parse length is 3 (EX: Genesis 1:2)
         elif len(parsed_passage) == 3:
             init_verse = parsed_passage[2]  # the number after the colon 
             verse = data[book][chapter][init_verse]
             singleverse = parsed_passage[2] + ' ' + verse  # Getting proper format 
             obt_passage.append(singleverse)
             return readlist(obt_passage)
-        # if parse length is 6 (EX: Genesis 1:2-3)
+        # if parse length is 4 (EX: Genesis 1:2-3)
         elif len(parsed_passage) == 4:
             # defining variables
             end_verse = parsed_passage[3]
@@ -39,15 +38,26 @@ def getverse(passage):
         elif parsed_passage == 0:
             return "Not proper format (Book Chapter:list or Book Chapter)"
     except KeyError:
-        return "That passage does not exist"  # If there is no passage that exists
+        return "Not proper format (Book Chapter:list or Book Chapter)"  # If there is no passage that exists
 
-
+# Allows user to get random verse from any book
 def getrandomverse():
     passage = randompassage()
+    return f'{passage}:\n{getverse(passage)}'
+
+# Allows user to get random verse from a specific book 
+def randompassage_withbook(book):
+    passage = randombook_passage(book)
     return f'{passage}:\n{getverse(passage)}'
 
 def readlist(list):
     string = ' \n \n'.join(list)
     return string
         
+def booklist():
+    old_testament = '\n'.join(books[0:46])
+    new_testament = ' \n'.join(books[46:])
+    return f"Old Testament:\n\n{old_testament}\n\nNew Testament:\n\n{new_testament}"
+
+
 
